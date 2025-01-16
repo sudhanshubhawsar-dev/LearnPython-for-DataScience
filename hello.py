4403e4e2821ab5fe16c852d2d776b281bb8e858d
Sudhanshu Bhawsar <sudhanshubhawsar68@gmail.com>
2025-01-16 23:48:53 +0530

    python begins

---
+++ hello.py
@@ -0,0 +1,27 @@
+print("Hello, Data Science")
+
+# Numbers
+a = 10       # Integer
+b = 3.14     # Float
+c = 3 + 4j   # Complex number
+
+# Strings
+name = "Sudhanshu"
+print(f"My name is {name}")
+
+# Boolean
+is_data_scientist = True
+
+# NoneType
+nothing = None
+print(type(nothing))
+
+# Input example
+name = input("What is your name? ")
+print(f"Hello, {name}!")
+
+age = 28
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are a minor.")
